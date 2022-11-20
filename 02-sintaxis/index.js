7881e8371e2e0c1b881e809fe60617b00a49f1a6
DanielDominguez <danieljdominguezb@hotmail.com>
2022-11-20 00:17:30 -0500

    Ejercicio 2

---
+++ 02-sintaxis/index.js
@@ -0,0 +1,11 @@
+const ahora = new Date("July 11 2002");
+const favoryBook = {
+    titulo: "Harry Potter y la piedra filosofal",
+    autor: "J.K rowling",
+    fecha: new Date("June 26 1997"),
+    urlBook: "https://www.buscalibre.com.co/libro-harry-potter-y-la-piedra-filosofal-ed-minalima-harry-potter-1-j-k-rowling-salamandra-infantil-y-juvenil/9788418174070/p/53071491"
+}
+
+const list = ["Daniel", 20, false, ahora, favoryBook];
+
+console.log(list);
