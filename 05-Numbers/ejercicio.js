3b2b898ff02b32903a76083ac5331f44e2b733e9
DanielDominguez <danieljdominguezb@hotmail.com>
2022-12-21 14:24:09 -0500

    Ejercicio sesion 5

---
+++ 05-Numbers/ejercicio.js
@@ -0,0 +1,25 @@
+// Altura en centimetros
+let altcm = 1.70 * 100;
+console.log(altcm);
+
+// Altura en metros
+let altm = 1.70;
+
+// Peso en kg 
+let peso = 58.2;
+
+let alturaHaciaArriba = Math.round(altm);
+console.log(alturaHaciaArriba);
+
+let alturaHaciaAbajo = Math.floor(altm);
+console.log(alturaHaciaAbajo);
+
+let numeroMax = Number.MAX_VALUE + 1;
+
+let numeroMax2 = Number.MAX_VALUE;
+
+if (numeroMax === numeroMax2){
+    console.log("Felicidades Crack");
+} else {
+    console.log("no es igual bro");
+}
