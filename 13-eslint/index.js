f05b7f6c20d110068aaa22789fa8a052bfe5dc9d
DanielDominguez <danieljdominguezb@hotmail.com>
2022-12-25 20:44:03 -0500

    Ejercicio de la sesion 13

---
+++ 13-eslint/index.js
@@ -0,0 +1,25 @@
+let nombre = " Daniel";
+let lastName = " Dominguez ";
+let student = nombre.concat(lastName);
+console.log(student);
+
+let studentUpperCase = student.toUpperCase();
+console.log(studentUpperCase);
+
+let studentLowerCase = student.toLowerCase();
+console.log(studentLowerCase);
+
+let numLetra = student.length;
+console.log(numLetra);
+
+let firtWord = nombre.charAt(0);
+console.log(firtWord);
+
+console.log(lastName.charAt(9));
+
+console.log(student.trim().length);
+
+console.log(student.includes(nombre));
+
+console.log("hola");
+const hogar = "daniel"; // eslint-disable-line
