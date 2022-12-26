69b0e1064875cbb9155dcc768ee6d232d035172c
DanielDominguez <danieljdominguezb@hotmail.com>
2022-12-26 08:52:48 -0500

    Ejercicio 15 del curso Js

---
+++ 15-persistencia/index.js
@@ -0,0 +1,12 @@
+// let name = "Daniel";
+// let lastname = "Dominguez";
+
+// const persona = {
+//     nombre: "Daniel",
+//     apellido: "Dominguez"
+// }
+
+// sessionStorage.setItem("Persona", JSON.stringify(persona));
+// localStorage.setItem("persona", JSON.stringify(persona));
+
+// document.cookie = `caducida=${JSON.stringify(persona)};expires=` + new Date(2022, 11, 26, 08, 50).toUTCString()
